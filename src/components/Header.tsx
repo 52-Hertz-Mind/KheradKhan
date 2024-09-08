fddd2b81952675a200f7ba7b3789dd8222e03ac2
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-08 10:49:36 +0330

    #1 implement header (#2)
    * [#1-ADD] button and fix tailwind
    
    * [#1-UPDATE] header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 function Header(){
     return (
-        <button
-            className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
-            Click me
-        </button>
+        <div className='p-5 px-96 flex items-center justify-between'>
+            <div>Logo</div>
+            <button
+                className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
+                Sign up
+            </button>
+        </div>
+
     )
 }
 
