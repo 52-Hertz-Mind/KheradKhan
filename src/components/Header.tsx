89ccc42ccc732ea109014dcddf2c786da8d6cec7
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-08 10:28:28 +0330

    [#1-ADD] button and fix tailwind (#1)

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header(){
+    return (
+        <button
+            className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
+            Click me
+        </button>
+    )
+}
+
+export default Header
