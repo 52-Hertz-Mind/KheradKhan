be506f9c15f7817ef62b67e179c685f105278e60
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-09-10 11:24:47 +0330

    [#1-UPDATE] header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 function Header(){
     return (
         <div className='p-5 px-96 flex items-center justify-between'>
-            <div>Logo</div>
+            <div>لوگو</div>
             <button
                 className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
-                Sign up
+                ثبت نام
             </button>
         </div>
 
