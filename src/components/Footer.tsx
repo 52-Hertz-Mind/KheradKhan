1aa5c661053a5e90776d712c60ec8a3088482d29
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-14 22:01:43 +0330

    #3 implement footer (#7)
    * [#3-IMPLEMENT] footer
    
    * [#3-UPDATE] font

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer(){
+    return(
+        <footer className='px-40 py-20 bg-gray-950 flex flex-col gap-10 font-MyFont'>
+            <div className='text-white flex flex-col gap-5' dir='rtl'>
+                <p className='text-3xl' >خردخوان</p>
+                <p> ما به هزاران خواننده کمک می کنیم تا از نکات برجسته دیجیتال خود بیشترین استفاده را ببرند.</p>
+            </div>
+            <div className='text-white flex gap-5' dir='rtl'>
+                <button className='hover:text-gray-300'>درباره ما</button>
+                <button className='hover:text-gray-300'>تماس با ما</button>
+                <button className='hover:text-gray-300'>داشبورد</button>
+                <button className='hover:text-gray-300'>قیمت ها</button>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': ['nunito', 'sans-serif'],
+        'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+      },
+    },
   },
   plugins: [],
 }
