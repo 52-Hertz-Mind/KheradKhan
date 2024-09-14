e3000cb5b92770c7db9d6557276f456f50d85c15
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-09-14 22:01:06 +0330

    [#3-UPDATE] font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer className='px-40 py-20 bg-gray-950 flex flex-col gap-10'>
+        <footer className='px-40 py-20 bg-gray-950 flex flex-col gap-10 font-MyFont'>
             <div className='text-white flex flex-col gap-5' dir='rtl'>
                 <p className='text-3xl' >خردخوان</p>
                 <p> ما به هزاران خواننده کمک می کنیم تا از نکات برجسته دیجیتال خود بیشترین استفاده را ببرند.</p>
