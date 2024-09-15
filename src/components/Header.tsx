5cea9fc9fbbe4734ebe53e6e549dfc5811c9c884
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-09-15 10:35:08 +0330

    [#3-FIX] font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header(){
     return (
         <div className='p-5 px-96 flex items-center justify-between'>
-            <div>لوگو</div>
+            <div className='text-2xl'>خردخوان</div>
             <button
                 className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
                 ثبت نام
