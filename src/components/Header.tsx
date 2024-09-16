89aa9caf2576e5998b6b1009c4aee8284fdf2299
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-09-16 19:08:44 +0330

    [#4-ADD] sign in popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,22 @@
+import SignIn from "./SignIn.tsx";
+import {useState} from "react";
+
 function Header(){
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className='p-5 px-96 flex items-center justify-between'>
-            <div className='text-2xl'>خردخوان</div>
-            <button
-                className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300">
-                ثبت نام
-            </button>
-        </div>
+        <>
+            <div className='p-5 px-96 flex items-center justify-between'>
+                <div className='text-2xl'>خردخوان</div>
+                <button
+                    className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-black hover:text-white transition-colors duration-300"
+                onClick={() => setIsOpen(!isOpen)}
+                >
+                    ثبت نام
+                </button>
+            </div>
+            <SignIn isOpen={isOpen} setIsOpen={setIsOpen} />
+        </>
+
 
     )
 }
