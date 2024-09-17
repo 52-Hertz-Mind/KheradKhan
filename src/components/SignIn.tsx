4d82c44d6f2318c1b9632593126bb064ed9aab14
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-17 09:24:03 +0330

    #4 implement signin (#10)
    * [#4-ADD] sign in popup
    
    * [#4-UPDATE] sign in componenet
    
    * [#4-ADD] email and password states

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,12 @@
+import {useState} from "react";
 
 
 function SignIn({isOpen, setIsOpen}){
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     function signIn(){
-        console.log("Signing in");
+        console.log(email, password);
         setIsOpen(false);
     }
     function closePopup(){
@@ -17,10 +20,10 @@ function SignIn({isOpen, setIsOpen}){
                 className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-96 h-2/3 w-1/3 bg-white rounded-2xl shadow-2xl p-20 z-20' onClick={(e) => e.stopPropagation()}>
                 <form onSubmit={signIn} className='flex flex-col gap-5' dir='rtl'>
                     <label htmlFor='email'>ایمیل</label>
-                    <input type='email' placeholder='ایمیل' className='p-2 border-2'/>
+                    <input required type='email' placeholder='ایمیل' className='p-2 border-2 rounded-2xl' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <label htmlFor='password'>گذرواژه</label>
-                    <input type='password' placeholder='گذرواژه' className='p-2 border-2'/>
-                    <button onClick={signIn}
+                    <input required type='password' placeholder='گذرواژه' className='p-2 border-2 rounded-2xl' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button onSubmit={signIn}
                             className='mt-10 bg-blue-500 rounded-2xl p-2 text-white hover:bg-blue-700 duration-150'>ثبت
                         نام
                     </button>
