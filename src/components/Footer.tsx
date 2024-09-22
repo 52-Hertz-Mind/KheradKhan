87008ae83d26d910e5fe177b552cb2f49a69fbe7
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-09-22 11:00:25 +0330

    [#6-UPDATE] footer i18n

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,47 @@
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
 function Footer() {
+  //region hooks
+  const [captions, setCaptions] = useState<HeaderCaptionsModel>();
   const [t, i18n] = useTranslation();
+  useEffect(() => {
+    setCaption();
+  }, []);
+  //endregion
 
+  //region functions
+  function setCaption() {
+    const headerCaption = t('footer', {
+      returnObjects: true,
+    }) as HeaderCaptionsModel;
+    setCaptions(headerCaption);
+  }
+  //endregion
+
+  //region template
+  if (!captions) return;
   return (
     <footer className="px-40 py-20 bg-gray-950 flex flex-col gap-10 font-MyFont">
       <div className="text-white flex flex-col gap-5" dir="rtl">
-        <p className="text-3xl">{t('name')}</p>
-        <p>
-          {' '}
-          ما به هزاران خواننده کمک می کنیم تا از نکات برجسته دیجیتال خود بیشترین
-          استفاده را ببرند.
-        </p>
+        <p className="text-3xl">{captions.name}</p>
+        <p>{captions.caption}</p>
       </div>
       <div className="text-white flex gap-5" dir="rtl">
-        <button className="hover:text-gray-300">درباره ما</button>
-        <button className="hover:text-gray-300">تماس با ما</button>
-        <button className="hover:text-gray-300">داشبورد</button>
-        <button className="hover:text-gray-300">قیمت ها</button>
+        <button className="hover:text-gray-300">{captions.aboutUs}</button>
+        <button className="hover:text-gray-300">{captions.contactUs}</button>
+        <button className="hover:text-gray-300">{captions.dashboard}</button>
+        <button className="hover:text-gray-300">{captions.pricing}</button>
       </div>
     </footer>
   );
+  //endregion
 }
 export default Footer;
+interface HeaderCaptionsModel {
+  name: string;
+  caption: string;
+  aboutUs: string;
+  contactUs: string;
+  dashboard: string;
+  pricing: string;
+}
