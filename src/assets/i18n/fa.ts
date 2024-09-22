e786c701ded578eb1ab7aa78e529569300cc846e
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-22 11:01:37 +0330

    #6 implement i18n (#14)
    * [#6-FIX] prettier
    
    * [#6-INIT] i18n
    
    * [#6-UPDATE] i18n
    
    * [#6-UPDATE] region structure
    
    * [#6-UPDATE] footer i18n

--- src/assets/i18n/fa.ts
+++ src/assets/i18n/fa.ts
@@ -3,4 +3,13 @@ export const faTranslation = {
     name: 'خردخوان',
     signIn: 'ثبت نام',
   },
+  footer: {
+    name: 'خردخوان',
+    caption:
+      'ما به هزاران خواننده کمک می کنیم تا از نکات برجسته دیجیتال خود بیشترین استفاده را ببرند.',
+    aboutUs: 'درباره ما',
+    contactUs: 'تماس با ما',
+    dashboard: 'داشبورد',
+    pricing: 'قیمت ها',
+  },
 };
