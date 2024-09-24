b63f3b9193846d697ae98dea430fe737c08b2225
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-09-24 11:31:35 +0330

    #6 implement i18n (#15)
    * [#6-FIX] prettier
    
    * [#6-INIT] i18n
    
    * [#6-UPDATE] i18n
    
    * [#6-UPDATE] region structure
    
    * [#6-UPDATE] footer i18n
    
    * [#6-UPDATE] hero i18n

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,47 @@
-function Hero(){
-    return(
-        <div className='flex flex-col gap-5 items-center justify-center '>
-            <div className='flex flex-col gap-2 justify-center items-center'>
-                <p className='text-3xl font-bold'>از آنچه می خوانید بیشترین بهره را ببرید</p>
-                <p>ما بازدید مجدد و یادگیری از نکات برجسته کتاب و مقاله الکترونیکی را آسان می کنیم</p>
-            </div>
-            <div>
-                <button className='bg-blue-500 rounded-2xl text-white p-2 w-40 hover:bg-blue-700 duration-150'>رایگان شروع کنید</button>
-            </div>
-            <div>
-                <img src='/src/assets/hero.png' className='h-[600px] w-[1000px]' />
-            </div>
-        </div>
-    )
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+function Hero() {
+  //region hooks
+  const [isOpen, setIsOpen] = useState(false);
+  const [captions, setCaptions] = useState<HeaderCaptionsModel>();
+  const [t, i18n] = useTranslation();
+  useEffect(() => {
+    setCaption();
+  }, []);
+  //endregion
+
+  //region functions
+  function setCaption() {
+    const headerCaption = t('hero', {
+      returnObjects: true,
+    }) as HeaderCaptionsModel;
+    setCaptions(headerCaption);
+  }
+  //endregion
+  //region template
+  if (!captions) return;
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center ">
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <p className="text-3xl font-bold">{captions.title}</p>
+        <p>{captions.subtitle}</p>
+      </div>
+      <div>
+        <button className="bg-blue-500 rounded-2xl text-white p-2 w-40 hover:bg-blue-700 duration-150">
+          {captions.startButton}
+        </button>
+      </div>
+      <div>
+        <img src="/src/assets/hero.png" className="h-[600px] w-[1000px]" />
+      </div>
+    </div>
+  );
+  //end region
 }
 export default Hero;
+
+interface HeaderCaptionsModel {
+  title: string;
+  subtitle: string;
+  startButton: string;
+}
