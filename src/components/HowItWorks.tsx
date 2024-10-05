af29a99dcf99745c274a470713a692e490dce9f4
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-10-05 07:38:25 +0330

    [#ADD-i18n] for how it works section

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,42 +1,87 @@
-function HowItWorks(){
-    return(
-        <div className='flex flex-col gap-8 mt-20 items-end p-40'>
-            <div>
-                <p className='text-5xl font-bold'>:نحوه کار سایت</p>
-            </div>
-            <div className='flex gap-5 w-full'>
-                {/*1*/}
-                <div className='flex flex-col gap-5 items-end w-1/3'>
-                    <div className='flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full' >
-                        <p className='text-2xl'>به یاد بیاورید</p>
-                        <img src='/src/assets/remember.png' className='size-20' alt='import image'/>
-                    </div>
-                    <p className='' dir='rtl'>بیشتر به یاد بیاورید و دانش خود <br/> را در طول زمان افزایش دهید.
-                    </p>
-                </div>
-                {/*2*/}
-                <div className='flex flex-col gap-5 items-end w-1/3'>
-                    <div className='flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full'>
-                        <p className='text-2xl'>مرور کنید</p>
-                        <img src='/src/assets/review.png' className='size-20' alt='import image'/>
-                    </div>
-                    <p className='' dir='rtl'>بهترین قسمت ها را به راحتی از طریق <br/> ایمیل و برنامه روزانه مرور کنید.
-                    </p>
-                </div>
-                {/*3*/}
-                <div className='flex flex-col gap-5 items-end w-1/3'>
-                    <div className='flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full'>
-                        <p className='text-2xl'>هایلات ها را وارد کنید</p>
-                        <img src='/src/assets/import.png' className='size-20' alt='import image'/>
-                    </div>
-                    <p className='' dir='rtl'> ها را از سایت ها<br/> و کتاب های مختلف وارد سایت کنید</p>
-                </div>
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
+function HowItWorks() {
+  //region hooks
+  const [captions, setCaptions] = useState<HowItWorksCaptionsModel>();
+  const [t, i18n] = useTranslation();
+  useEffect(() => {
+    setCaption();
+  }, []);
+  //end region
 
-            </div>
+  //region functions
+  function setCaption() {
+    const howItWorksCaption = t('howItWorks', {
+      returnObjects: true,
+    }) as HowItWorksCaptionsModel;
+    setCaptions(howItWorksCaption);
+  }
+  //end region
 
+  //region template
+  if (!captions) return;
+  return (
+    <div className="flex flex-col gap-8 mt-20 items-end p-40">
+      <div>
+        <p className="text-5xl font-bold">{captions.title}</p>
+      </div>
+      <div className="flex gap-5 w-full">
+        {/*1*/}
+        <div className="flex flex-col gap-5 items-end w-1/3">
+          <div className="flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full">
+            <p className="text-2xl">{captions.rememberTitle}</p>
+            <img
+              src="/src/assets/remember.png"
+              className="size-20"
+              alt="import image"
+            />
+          </div>
+          <p className="" dir="rtl">
+            {captions.rememberCaption}
+          </p>
         </div>
-    )
+        {/*2*/}
+        <div className="flex flex-col gap-5 items-end w-1/3">
+          <div className="flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full">
+            <p className="text-2xl">{captions.reviewTitle}</p>
+            <img
+              src="/src/assets/review.png"
+              className="size-20"
+              alt="import image"
+            />
+          </div>
+          <p className="" dir="rtl">
+            {captions.reviewCaption}
+          </p>
+        </div>
+        {/*3*/}
+        <div className="flex flex-col gap-5 items-end w-1/3">
+          <div className="flex gap-2 items-center justify-end border-b-2 border-b-black p-2 w-full">
+            <p className="text-2xl">{captions.importTitle}</p>
+            <img
+              src="/src/assets/import.png"
+              className="size-20"
+              alt="import image"
+            />
+          </div>
+          <p className="" dir="rtl">
+            {captions.importCaption}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+  //end region
 }
 
 export default HowItWorks;
+interface HowItWorksCaptionsModel {
+  title: string;
+  importTitle: string;
+  importCaption: string;
+  reviewTitle: string;
+  reviewCaption: string;
+  rememberTitle: string;
+  rememberCaption: string;
+}
