ef26cb3adcb3880de86787423e61518fb36934cc
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-10-16 20:41:09 +0330

    #10 implement import section (#25)
    * [#10-ADD] import button
    
    * [#10-ADD] export & books button

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,19 +1,38 @@
 import Footer from '../components/Footer.tsx';
 import DashboardHeader from '../components/DashboardHeader.tsx';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBook,
+  faDownload,
+  faFileExport,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Dashboard() {
   return (
     <div>
       <DashboardHeader />
-      <div dir="rtl" className="px-96 py-20">
+      <div dir="rtl" className="flex flex-col gap-5 h-screen px-96 py-20">
         <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
           <FontAwesomeIcon
             icon={faDownload}
             className="size-8 transition-transform duration-300 group-hover:scale-110"
           />
           <p className="group-hover:-translate-x-1 duration-150">وارد کردن</p>
+        </button>
+        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+          <FontAwesomeIcon
+            icon={faFileExport}
+            className="size-8 transition-transform duration-300 group-hover:scale-110"
+          />
+
+          <p className="group-hover:-translate-x-1 duration-150">خارج کردن</p>
+        </button>
+        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+          <FontAwesomeIcon
+            icon={faBook}
+            className="size-8 transition-transform duration-300 group-hover:scale-110"
+          />
+          <p className="group-hover:-translate-x-1 duration-150">کتاب ها</p>
         </button>
       </div>
       <Footer />
