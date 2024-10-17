e03058f452ab3013802cfcf9a287f581e5235ef0
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-10-17 11:10:53 +0330

    #10 implement import section (#26)
    * [#10-ADD] import button
    
    * [#10-ADD] export & books button
    
    * [#10-ADD] center div for import section
    
    * [#10-ADD] import pop-up

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -6,35 +6,47 @@ import {
   faFileExport,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ImportScreen from '../components/ImportScreen.tsx';
+import { useState } from 'react';
 
 function Dashboard() {
+  const [isImportScreenOpen, setIsImportScreenOpen] = useState(false);
   return (
     <div>
       <DashboardHeader />
-      <div dir="rtl" className="flex flex-col gap-5 h-screen px-96 py-20">
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faDownload}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
-          <p className="group-hover:-translate-x-1 duration-150">وارد کردن</p>
-        </button>
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faFileExport}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
+      <div dir="rtl" className="flex justify-center h-screen px-96 py-20">
+        <div className="flex flex-col gap-5 items-center border p-12 w-1/2">
+          <button
+            onClick={() => setIsImportScreenOpen(!isImportScreenOpen)}
+            className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group"
+          >
+            <FontAwesomeIcon
+              icon={faDownload}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
+            <p className="group-hover:-translate-x-1 duration-150">وارد کردن</p>
+          </button>
+          <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+            <FontAwesomeIcon
+              icon={faFileExport}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
 
-          <p className="group-hover:-translate-x-1 duration-150">خارج کردن</p>
-        </button>
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faBook}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
-          <p className="group-hover:-translate-x-1 duration-150">کتاب ها</p>
-        </button>
+            <p className="group-hover:-translate-x-1 duration-150">خارج کردن</p>
+          </button>
+          <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+            <FontAwesomeIcon
+              icon={faBook}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
+            <p className="group-hover:-translate-x-1 duration-150">کتاب ها</p>
+          </button>
+        </div>
       </div>
+      <ImportScreen
+        isOpen={isImportScreenOpen}
+        setIsOpen={setIsImportScreenOpen}
+      />
       <Footer />
     </div>
   );
