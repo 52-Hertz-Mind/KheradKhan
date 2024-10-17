d0de05943a03dda5dca2ba667c1b1d1afcfded0f
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-10-17 10:50:27 +0330

    [#10-ADD] center div for import section

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -11,29 +11,31 @@ function Dashboard() {
   return (
     <div>
       <DashboardHeader />
-      <div dir="rtl" className="flex flex-col gap-5 h-screen px-96 py-20">
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faDownload}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
-          <p className="group-hover:-translate-x-1 duration-150">وارد کردن</p>
-        </button>
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faFileExport}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
+      <div dir="rtl" className="flex justify-center h-screen px-96 py-20">
+        <div className="flex flex-col gap-5 items-center border p-12 w-1/2">
+          <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+            <FontAwesomeIcon
+              icon={faDownload}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
+            <p className="group-hover:-translate-x-1 duration-150">وارد کردن</p>
+          </button>
+          <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+            <FontAwesomeIcon
+              icon={faFileExport}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
 
-          <p className="group-hover:-translate-x-1 duration-150">خارج کردن</p>
-        </button>
-        <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
-          <FontAwesomeIcon
-            icon={faBook}
-            className="size-8 transition-transform duration-300 group-hover:scale-110"
-          />
-          <p className="group-hover:-translate-x-1 duration-150">کتاب ها</p>
-        </button>
+            <p className="group-hover:-translate-x-1 duration-150">خارج کردن</p>
+          </button>
+          <button className="flex gap-5 items-center w-96 p-2 rounded-2xl hover:bg-gray-200  duration-300 group">
+            <FontAwesomeIcon
+              icon={faBook}
+              className="size-8 transition-transform duration-300 group-hover:scale-110"
+            />
+            <p className="group-hover:-translate-x-1 duration-150">کتاب ها</p>
+          </button>
+        </div>
       </div>
       <Footer />
     </div>
