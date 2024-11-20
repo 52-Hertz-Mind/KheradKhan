8a54f824c0cf453708a4092249e5c92b1cc52ebb
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-11-20 19:20:37 +0330

    #12 implement highlights page (#35)
    * [#12-ADD] highlights page
    
    * [#12-UPDATE] highlights page

--- src/Pages/Highlights.tsx
+++ src/Pages/Highlights.tsx
@@ -1,9 +1,17 @@
 import DashboardHeader from '../components/DashboardHeader.tsx';
+import { useSelector } from 'react-redux';
+import { RootState } from '../state/store.ts';
 
 function Highlights() {
+  const books = useSelector((state: RootState) => state.books.books);
   return (
     <div>
       <DashboardHeader />
+      {books.map((book, index) => (
+        <div key={index}>
+          <p>{book.highlightText}</p>
+        </div>
+      ))}
     </div>
   );
 }
