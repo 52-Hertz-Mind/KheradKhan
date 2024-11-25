85b998424bfaec8f60ec4769a0497b190e211eb1
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-11-25 16:45:46 +0330

    #12 implement highlights page (#40)
    * [#12-ADD] highlights page
    
    * [#12-UPDATE] highlights page
    
    * [#12-UPDATE] highlights page
    
    * [#12-ADD] add footer to components
    
    * [#12-ADD] showing selected book highlights  function
    
    * [#12-UPDATE] books component
    
    * [#12-UPDATE] books comments
    
    * [#12-UPDATE] highlight appearance
    
    * [#12-ADD] back button

--- src/Pages/Highlights.tsx
+++ src/Pages/Highlights.tsx
@@ -1,27 +1,35 @@
-import DashboardHeader from '../components/DashboardHeader.tsx';
 import { useSelector } from 'react-redux';
 import { RootState } from '../state/store.ts';
 import Footer from '../components/Footer.tsx';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Highlights() {
   const location = useLocation();
   const { selectedBookId } = location.state || {};
+  const navigate = useNavigate();
   const books = useSelector((state: RootState) => state.books.books);
 
   const filteredBooks = books.filter((book) => book.id === selectedBookId);
   return (
-    <div>
-      <DashboardHeader />
-      <div className="p-10">
+    <div className="bg-gray-200">
+      <div className="px-96 pt-10 flex items-center justify-between" dir="rtl">
+        <button onClick={() => navigate('/books')}>
+          <FontAwesomeIcon className="size-8" icon={faArrowRight} />
+        </button>
+      </div>
+      <div className="p-10 flex flex-col justify-center items-center" dir="rtl">
+        {filteredBooks.map((book) => (
+          <p className="font-bold text-2xl">{book.bookName}</p>
+        ))}
         {filteredBooks.length > 0 ? (
           filteredBooks.map((book) => (
             <div
-              className="size-1/3 h-96 m-auto mt-10 mb-10 text-white rounded-2xl p-10 bg-gray-600"
+              className="size-1/3 h-96 m-auto mt-10 mb-10 text-gray-800 rounded-2xl p-10 bg-white"
               key={book.id}
             >
               <p>{book.highlightText}</p>
-              <p>{book.id}</p>
             </div>
           ))
         ) : (
