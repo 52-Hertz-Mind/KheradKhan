1c40ec8308f32163fa6c7903b522d19fa80f072e
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-11-26 18:34:54 +0330

    [#14-UPDATE] book data & component
    add image data and implement in books component

--- src/Pages/Books.tsx
+++ src/Pages/Books.tsx
@@ -15,27 +15,30 @@ function Books() {
   //endregion
 
   return (
-    <div>
+    <>
       <DashboardHeader />
-      <div
-        className="grid grid-cols-4 gap-5 p-40 items-center border w-screen h-full"
-        dir="rtl"
-      >
-        {books.map((book, index) => (
-          <div
-            className="size-full border p-10 cursor-pointer"
-            onClick={() => {
-              navigateToBook(book.id);
-            }}
-          >
-            <button className="flex flex-col" key={index}>
-              {book.bookName}
-            </button>
-          </div>
-        ))}
+      <div dir="rtl" className="flex flex-col items-start">
+        <div className="size-1/2 mx-auto mt-10">
+          <h1 className="text-5xl ">کتاب ها</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-5 py-20 px-96 items-center w-screen h-full">
+          {books.map((book, index) => (
+            <div
+              className="flex flex-col gap-5 size-full border p-10 cursor-pointer shadow hover:shadow-2xl duration-300"
+              onClick={() => {
+                navigateToBook(book.id);
+              }}
+            >
+              <img src={book.bookImage} alt={book.bookName} />
+              <p className="flex flex-col" key={index}>
+                {book.bookName}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
