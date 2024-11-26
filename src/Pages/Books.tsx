ab2d05e0f70058aa39394d3b328a7cd421b909f4
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-11-26 18:35:26 +0330

    #14 update books page design (#45)
    * [#14-UPDATE] book component
    
    * [#14-UPDATE] book data & component
    
    add image data and implement in books component

--- src/Pages/Books.tsx
+++ src/Pages/Books.tsx
@@ -8,35 +8,37 @@ function Books() {
   const books = useSelector((state: RootState) => state.books.books);
   const navigate = useNavigate();
 
-  //region function
+  //region functions
   function navigateToBook(id: string) {
-    // navigate('/books/highlights', {
-    //   state: {
-    //     selectedBookId: id,
-    //   },
-    // });
     navigate(`/books/highlights/${id}`);
   }
   //endregion
 
   return (
-    <div>
+    <>
       <DashboardHeader />
-      <div className="flex flex-col gap-5 p-10 items-center border w-screen h-full">
-        {books.map((book, index) => (
-          <button
-            onClick={() => {
-              navigateToBook(book.id);
-            }}
-            className="flex flex-col"
-            key={index}
-          >
-            {book.bookName}
-          </button>
-        ))}
+      <div dir="rtl" className="flex flex-col items-start">
+        <div className="size-1/2 mx-auto mt-10">
+          <h1 className="text-5xl ">کتاب ها</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-5 py-20 px-96 items-center w-screen h-full">
+          {books.map((book, index) => (
+            <div
+              className="flex flex-col gap-5 size-full border p-10 cursor-pointer shadow hover:shadow-2xl duration-300"
+              onClick={() => {
+                navigateToBook(book.id);
+              }}
+            >
+              <img src={book.bookImage} alt={book.bookName} />
+              <p className="flex flex-col" key={index}>
+                {book.bookName}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/state/books/booksSlice.ts
+++ src/state/books/booksSlice.ts
@@ -3,6 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface BookValue {
   bookName: string;
   highlightText: string;
+  bookImage: string;
   id: string;
 }
 interface BooksState {
@@ -14,25 +15,36 @@ const initialState: BooksState = {
       bookName: 'بی‌شعوری',
       highlightText:
         'بی‌شعوری، نوعی اختلال رفتاری است که فرد در اثر آن نمی‌تواند با دیگران به شکلی محترمانه و انسانی رفتار کند.',
+      bookImage: '/bookImageSample.png',
       id: '1',
     },
     {
       bookName: 'هنر شفاف اندیشیدن',
       highlightText:
         'ما انسان‌ها معمولاً بیشتر به احساساتمان وابسته‌ایم تا به تفکر منطقی، و این موضوع تصمیم‌گیری‌های ما را تحت تأثیر قرار می‌دهد.',
+      bookImage: '/bookImageSample.png',
       id: '2',
     },
     {
       bookName: 'انسان در جستجوی معنا',
       highlightText:
         'معنای زندگی در شرایط سخت، در توانایی ما برای یافتن هدفی عمیق‌تر نهفته است.',
+      bookImage: '/bookImageSample.png',
       id: '3',
     },
     {
       bookName: 'چه کسی پنیر مرا جابجا کرد؟',
       highlightText:
         'تغییر اجتناب‌ناپذیر است، و کسانی که با آن تطبیق پیدا کنند، موفق‌تر خواهند بود.',
+      bookImage: '/bookImageSample.png',
       id: '4',
+    },
+    {
+      bookName: 'چه کسی پنیر مرا جابجا کرد2؟',
+      highlightText:
+        'تغییر اجتناب‌ناپذیر است، و کسانی که با آن تطبیق پیدا کنند، موفق‌تر خواهند بود.',
+      bookImage: '/bookImageSample.png',
+      id: '5',
     },
   ],
 };
