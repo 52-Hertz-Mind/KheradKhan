b1ec107e705a41eb2fa7e66a2a5e89a841e38c14
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-12-09 11:29:11 +0330

    [#23-UPDATE] review component

--- src/Pages/Review.tsx
+++ src/Pages/Review.tsx
@@ -1,20 +1,37 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../state/store.ts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Review() {
   const books = useSelector((state: RootState) => state.books.books);
-  const [randomHighlights, setRandomHighlights] = useState();
+  const [randomHighlights, setRandomHighlights] = useState<string[]>();
 
-  function getRandomHighlights(): string[] {
-    const allHighlights = [];
-    books.map((book) => {
-      allHighlights.push(book.highlightText);
+  function getRandomHighlights(count: number): string[] {
+    const allHighlights: string[] = [];
+    books.flatMap((book) => {
+      book.highlightText.map((highlight) => {
+        allHighlights.push(highlight);
+      });
     });
-    console.log(allHighlights);
-    return allHighlights;
+    const shuffledHighlights = allHighlights.sort(() => 0.5 - Math.random());
+    console.log(shuffledHighlights);
+    return shuffledHighlights.slice(0, count);
   }
 
-  return <div>{getRandomHighlights()}</div>;
+  useEffect(() => {
+    const highlights = getRandomHighlights(3);
+    setRandomHighlights(highlights);
+  }, [books]);
+
+  return (
+    <div className="flex flex-col items-center justify-center" dir="rtl">
+      {randomHighlights?.map((highlight, index) => (
+        <div className="flex p-20 size-1/2 bg-gray-200 mt-10 rounded-2xl shadow-xl hover:shadow-2xl duration-300">
+          <p key={index}>{highlight}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 export default Review;
