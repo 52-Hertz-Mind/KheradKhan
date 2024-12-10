36af949d6a613edff51211fe25efe4410872f259
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-12-10 19:07:12 +0330

    [#26-UPDATE] dashboard navbar

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -1,27 +1,57 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import { useEffect, useRef, useState } from 'react';
 function DashboardNavbar() {
+  const [isUserPopUpOpen, setIsUserPopUpOpen] = useState(false);
+  const popupRef = useRef<HTMLDivElement>(null);
+  function handleUserPopup() {
+    setIsUserPopUpOpen((prevState) => !prevState);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popupRef.current &&
+        !popupRef.current.contains(event.target as Node)
+      ) {
+        setIsUserPopUpOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   return (
     <div className="p-5 px-96 flex items-center gap-5 justify-between">
       <div className="flex items-center gap-5">
-        <button
-          className="border-gray-400 w-6 h-6 rounded-full flex items-center justify-center transition-colors duration-200 border-2"
-          aria-label="Account"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="gray"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-6 h-6"
+        <div>
+          <button className="relative" onClick={handleUserPopup}>
+            <MdOutlineAccountCircle
+              size="25"
+              className="text-gray-600 hover:text-gray-950 duration-150"
+            />
+          </button>
+          {/*popup*/}
+          <div
+            ref={popupRef}
+            className={`rounded absolute shadow flex flex-col items-center gap-5 transition-all duration-150 ease-in-out ${isUserPopUpOpen ? 'opacity-100 scale-100 pointer-events-auto' : 'opacity-0 scale-75 pointer-events-none'}`}
           >
-            <circle color="currentColor" cx="12" cy="7" r="4" />
-            <path d="M6 21v-2a6 6 0 0 1 12 0v2" />
-          </svg>
-        </button>
+            <button className="hover:bg-gray-200 size-full p-2 px-5">
+              تنظیمات حساب
+            </button>
+            <button className="hover:bg-gray-200 size-full p-2 px-5">
+              تنظیمات مرور
+            </button>
+            <button className="hover:bg-gray-200 size-full p-2 px-5">
+              خروج از حساب
+            </button>
+          </div>
+        </div>
+
         {/* thunder button */}
         <button
           className="w-10 h-10 flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-sm"
